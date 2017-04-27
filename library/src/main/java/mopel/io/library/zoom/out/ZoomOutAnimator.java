1aeb202cbbacb21dde7aa5f497e5121f15837b3f
mopel <amosbake@gmail.com>
2017-04-27 22:59:54 +0800

    add ZoomIn Animator

---
+++ library/src/main/java/mopel/io/library/zoom/out/ZoomOutAnimator.java
@@ -0,0 +1,19 @@
+package mopel.io.library.zoom.out;
+
+import android.animation.ObjectAnimator;
+import android.view.View;
+import mopel.io.library.BaseAnimator;
+
+/**
+ * Author: mopel
+ * Date : 2017/4/25
+ * cssAnimation: https://github.com/daneden/animate.css/blob/master/source/zooming_entrances/zoomOut.css
+ */
+
+public class ZoomOutAnimator extends BaseAnimator {
+  @Override protected void prepare(View target) {
+    getAnimator().playTogether(ObjectAnimator.ofFloat(target, "scaleX", 1f, .3f, 0f),
+        ObjectAnimator.ofFloat(target, "scaleY", 1f, .3f, 0f),
+        ObjectAnimator.ofFloat(target, "alpha", 1f, 0f, 0f));
+  }
+}
