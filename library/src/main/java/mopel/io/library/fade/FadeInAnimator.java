18695beaaf86d3ee2a5e3eb7d2e6f294bf3b4a79
mopel <amosbake@gmail.com>
2017-04-29 22:27:56 +0800

    add FadeInAnimator

---
+++ library/src/main/java/mopel/io/library/fade/FadeInAnimator.java
@@ -0,0 +1,17 @@
+package mopel.io.library.fade;
+
+import android.animation.ObjectAnimator;
+import android.view.View;
+import mopel.io.library.BaseAnimator;
+
+/**
+ * Author: mopel
+ * Date : 2017/4/25
+ * cssAnimation: https://github.com/daneden/animate.css/blob/master/source/fading_entrances/fadeIn.css
+ */
+
+public class FadeInAnimator extends BaseAnimator {
+  @Override protected void prepare(View target) {
+    getAnimator().play(ObjectAnimator.ofFloat(target, "alpha", 0, 1));
+  }
+}
---
+++ library/src/main/java/mopel/io/library/fade/FadeInLeftAnimator.java
@@ -0,0 +1,18 @@
+package mopel.io.library.fade;
+
+import android.animation.ObjectAnimator;
+import android.view.View;
+import mopel.io.library.BaseAnimator;
+
+/**
+ * Author: mopel
+ * Date : 2017/4/25
+ * cssAnimation: https://github.com/daneden/animate.css/blob/master/source/fading_entrances/fadeInLeft.css
+ */
+
+public class FadeInLeftAnimator extends BaseAnimator {
+  @Override protected void prepare(View target) {
+    getAnimator().playTogether(ObjectAnimator.ofFloat(target, "alpha", 0, 1),
+        ObjectAnimator.ofFloat(target, "translationX", -target.getWidth(), 0));
+  }
+}
